019ad98d07635e9736ddbf263e7f0b7e8ac08215
flowihs <zmulenko500@gmail.com>
2026-06-15 21:09:42 +0300

    update sidebar

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -12,9 +12,11 @@ import playlistImage from "@/public/header/navigation/playlist-image.png";
 import {useEffect} from "react";
 import {usePlaylistStore} from "@/store/playlistStore";
 import {useModalStore} from "@/store/modalStore";
+import {useGlobalStore} from "@/store/globalStore";
 
 export default function Sidebar() {
   const pathname = usePathname();
+  const user = useGlobalStore((state) => state.user);
   const { playlists, fetchPlaylists } = usePlaylistStore();
   const openAddPlaylistModal: () => void = useModalStore((state) => state.openAddPlaylistModal);
 
@@ -49,48 +51,49 @@ export default function Sidebar() {
           );
         })}
       </div>
-
-      <section className={styles.library}>
-        <div className={styles.libraryTop}>
-          <div className={styles.libraryHeader}>
-            <h2 className={styles.libraryTitle}>Мои библиотеки</h2>
-            <button
-              onClick={() => openAddPlaylistModal()}
-              className={styles.libraryAddBtn}
-              type="button"
-            >
-              <Plus className={styles.libraryAddIcon} size={18} strokeWidth={2.5} />
-            </button>
-          </div>
-
-          <div className={clsx(styles.search, styles.librarySearch)}>
-            <Image className={clsx(styles.searchIcon, styles.librarySearchIcon)} src={search} alt="" />
-            <input
-              className={clsx(styles.searchInput, styles.librarySearchInput)}
-              type="text"
-              placeholder="Введите текст..."
-            />
-          </div>
-        </div>
-
-        <ul className={styles.playlistList}>
-          {playlists.map((playlist) => (
-            <li className={styles.playlistItem} key={playlist.id}>
-              <Image
-                className={styles.playlistCover}
-                src={playlistImage}
-                alt=""
-                width={52}
-                height={52}
-              />
-              <div className={styles.playlistInfo}>
-                <p className={styles.playlistName}>{playlist.name}</p>
-                <p className={styles.playlistMeta}>{"1"} треков</p>
+      {user && (
+          <section className={styles.library}>
+            <div className={styles.libraryTop}>
+              <div className={styles.libraryHeader}>
+                <h2 className={styles.libraryTitle}>Мои библиотеки</h2>
+                <button
+                    onClick={() => openAddPlaylistModal()}
+                    className={styles.libraryAddBtn}
+                    type="button"
+                >
+                  <Plus className={styles.libraryAddIcon} size={18} strokeWidth={2.5} />
+                </button>
               </div>
-            </li>
-          ))}
-        </ul>
-      </section>
+
+              <div className={clsx(styles.search, styles.librarySearch)}>
+                <Image className={clsx(styles.searchIcon, styles.librarySearchIcon)} src={search} alt="" />
+                <input
+                    className={clsx(styles.searchInput, styles.librarySearchInput)}
+                    type="text"
+                    placeholder="Введите текст..."
+                />
+              </div>
+            </div>
+
+            <ul className={styles.playlistList}>
+              {playlists.map((playlist) => (
+                  <li className={styles.playlistItem} key={playlist.id}>
+                    <Image
+                        className={styles.playlistCover}
+                        src={playlistImage}
+                        alt=""
+                        width={52}
+                        height={52}
+                    />
+                    <div className={styles.playlistInfo}>
+                      <p className={styles.playlistName}>{playlist.name}</p>
+                      <p className={styles.playlistMeta}>{"1"} треков</p>
+                    </div>
+                  </li>
+              ))}
+            </ul>
+          </section>
+      )}
     </nav>
   );
 }
